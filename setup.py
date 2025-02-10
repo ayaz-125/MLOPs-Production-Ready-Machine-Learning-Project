90ebe880af7baed35bc6f0aa12949ffa9c9c9140
ayaz-125 <ayazr425@gmail.com>
2025-02-10 06:06:40 +0530

    requirements.txt completed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = "us_visa",       # Name of the package (your project name)
+    version = "0.0.0",      # Initial version of the package
+    author = 'Ayaz',        # Your name as the package author
+    author_email="ayazr425@gmail.com",  # Your email address
+    packages=find_packages()  # Automatically finds all Python packages inside the project
+)
